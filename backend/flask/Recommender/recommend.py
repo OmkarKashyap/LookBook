05b21cde7dd8a847e843404634cd7da5ac2b0c1e
Omkar Kashyap <omkar.m.kashyap@gmail.com>
2024-04-08 11:19:42 +0530

    Few changes

--- backend/flask/Recommender/recommend.py
+++ backend/flask/Recommender/recommend.py
@@ -1,14 +1,9 @@
 from image_utils import extract_features, load_feature_list, load_filenames, recommend
 
 def get_recommendations(features):
-    # Load precomputed features and filenames
     feature_list = load_feature_list()
     filenames = load_filenames()
-
-    # Get recommendations based on the extracted features
     indices = recommend(features)
-
-    # Prepare recommendation filenames
     recommended_filenames = [filenames[i] for i in indices[0]]
-
+    print(recommended_filenames)
     return recommended_filenames
